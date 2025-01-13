5f9a8d757e26bf454163dcb626ab1cb89b45e23e
GuanMac <guanpersonalproject@gmail.com>
2025-01-13 13:53:27 +0800

    feat: car の変数を private にする

--- Game/AlgorithmImplement/Car.py
+++ Game/AlgorithmImplement/Car.py
@@ -3,17 +3,23 @@ from Game.Commom.Vector2 import Vector2
 
 class Car:
     def __init__(self):
-        self.position = Vector2(0, 0)
-        self.velocity = 1
-        self.forward_angle = 0
-        self.steer_angle = 0
-        self.center_to_head = Vector2(1, 0)
-        self.center_to_tail = Vector2(-1, 0)
+        self.__position = Vector2(0, 0)
+        self.__velocity = 1
+        self.__forward_angle = 0
+        self.__steer_angle = 0
+        self.__center_to_head = Vector2(1, 0)
+        self.__center_to_tail = Vector2(-1, 0)
 
+    def get_position(self):
+        return self.__position
+    def get_velocity(self):
+        return self.__velocity
     def update_frame(self):
         move_offset = self._get_move_offset()
-        self.position += move_offset
+        self.__position += move_offset
 
     def _get_move_offset(self):
-       return Vector2(self.velocity,0)
+       return Vector2(self.__velocity, 0)
 
+    def set_steer_angle(self, angle):
+        self.__steer_angle = angle
--- Game/Test/Test/CarTest.py
+++ Game/Test/Test/CarTest.py
@@ -9,13 +9,13 @@ class CarTest(unittest.TestCase):
 
     def test_generate_car(self):
         car = Car()
-        self.assertEqual(True, car.velocity == 1)
+        self.assertEqual(True, car.get_velocity() == 1)
 
     def test_car_update(self):
         car = Car()
-        self.assertEqual(True, car.position.x == 0)
+        self.assertEqual(True, car.get_position().x == 0)
         car.update_frame()
-        self.assertEqual(True, car.position.x == 1)
+        self.assertEqual(True, car.get_position().x == 1)
 
 
 if __name__ == '__main__':
